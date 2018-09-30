491fb44fc0eb412080ec89d23381f299226d7c88
argaitt <argaitt@gmail.com>
2018-09-30 17:14:33 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let strLocal = str, tmp;
+  for (let i = 0, len = bracketsConfig.length; i < len; i++){
+      while ( strLocal.indexOf(bracketsConfig[i][1], strLocal.indexOf(bracketsConfig[i][0]) + 1) != -1) {
+        strLocal = strLocal.slice(0, strLocal.indexOf(bracketsConfig[i][0])) + strLocal.slice(strLocal.indexOf(bracketsConfig[i][0])+1);
+        strLocal = strLocal.slice(0, strLocal.indexOf(bracketsConfig[i][1])) + strLocal.slice(strLocal.indexOf(bracketsConfig[i][1])+1);
+      }
+      if (strLocal.indexOf(bracketsConfig[i][1]) != -1){
+        return false;
+      }
+  }
+  return true;
 }
